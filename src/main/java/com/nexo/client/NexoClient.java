d3797d4d4b71c2b59b0a9b4fecfff822080881f7
ZoeWithTheE <zoe@mniz.xyz>
2026-03-30 11:33:49 -0700

    Add initial delay for hello message retries to improve connection stability on velocity servers

--- src/main/java/com/nexo/client/NexoClient.java
+++ src/main/java/com/nexo/client/NexoClient.java
@@ -40,6 +40,7 @@ public final class NexoClient implements ClientModInitializer {
     private static final int STATUS_INCOMPATIBLE = 1;
     private static final int STATUS_NO_PERMISSION = 2;
 
+    private static final int HELLO_INITIAL_DELAY_TICKS = 10;
     private static final int HELLO_RETRY_TICKS = 20;
     private static final int MAX_HELLO_ATTEMPTS = 6;
     private static final int MAX_DYNAMIC_GROUP_TABS = 8;
@@ -82,8 +83,7 @@ public final class NexoClient implements ClientModInitializer {
             registryReceived = false;
             pendingGroupRefresh = hadSyncedContent;
             helloAttempts = 0;
-            helloRetryCountdown = 0;
-            sendHello("join");
+            helloRetryCountdown = HELLO_INITIAL_DELAY_TICKS;
         });
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
